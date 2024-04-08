126ea3b32d814d906c53a25afac868230bede52c
TonyTang <435853363@qq.com>
2024-04-08 09:00:25 +0800

    Feat: 1. 地图编辑器项目初创

---
+++ MapEditor/Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,17 @@
+/*
+ * Description:             GameLauncher.cs
+ * Author:                  TonyTang
+ * Create Date:             2024/04/08
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GameLauncher.cs
+/// ÓÎÏ·Æô¶¯Æ÷
+/// </summary>
+public class GameLauncher : MonoBehaviour {
+
+}
